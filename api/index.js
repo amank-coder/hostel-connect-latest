c99280e6676bc1928488a21c79a43d07b00ee3d8
Aman Konchgamey <amanmaxwel@gmail.com>
2024-03-09 12:10:06 +0530

    checking

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 3000;
 const app= express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'))
 
