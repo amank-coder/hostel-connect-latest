685a4d6805f9e2a16a40fed9da7e50d4dd26e1a1
Aman Konchgamey <amanmaxwel@gmail.com>
2024-03-15 10:41:03 +0530

    sidebar implemented

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Layout from '../components/layout/Layout'
+import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
+  
+  const { user } = useSelector(state=>state.auth);
   return (
-    <div>Dashboard</div>
+    <Layout>
+      <div className='p-4'>
+        <h1 className='text-3xl'>Welcome, <span className='text-blue-600'>{user?.name}</span> !</h1>
+      </div>
+    </Layout>
   )
 }
 
